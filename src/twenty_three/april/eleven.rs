1c51378186d1c24e4646be44884c4630e32ce33d
成路 <48679486+chenglu4343@users.noreply.github.com>
2023-04-11 07:01:05 +0000

    🦄 refactor: rename

---
+++ src/twenty_three/april/eleven.rs
@@ -0,0 +1,15 @@
+#[allow(unused)]
+fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::twenty_three::april::eleven::add_two;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(add_two(2), 4);
+    }
+}
+
