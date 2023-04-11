57896a5705ce5d142f4dac33f78ad9f95d444e61
成路 <48679486+chenglu4343@users.noreply.github.com>
2023-04-11 07:38:24 +0000

    ✨ feat: add 4-11

--- src/twenty_three/april/eleven.rs
+++ src/twenty_three/april/eleven.rs
@@ -1,15 +1,37 @@
-#[allow(unused)]
-fn add_two(a: i32) -> i32 {
-    a + 2
-}
+// https://leetcode.cn/problems/robot-bounded-in-circle/
 
-#[cfg(test)]
-mod tests {
-    use crate::twenty_three::april::eleven::add_two;
+struct Solution;
 
-    #[test]
-    fn exploration() {
-        assert_eq!(add_two(2), 4);
+impl Solution {
+    // copilot写的
+    pub fn is_robot_bounded(instructions: String) -> bool {
+        let mut x = 0;
+        let mut y = 0;
+        /* 0 直行， 1 右转， 2 后退 3 左转 */
+        let mut direction = 0;
+        for instruction in instructions.chars() {
+            match instruction {
+                'G' => {
+                    match direction {
+                        0 => y += 1,
+                        1 => x += 1,
+                        2 => y -= 1,
+                        3 => x -= 1,
+                        _ => (),
+                    }
+                }
+                'L' => direction = (direction + 3) % 4,
+                'R' => direction = (direction + 1) % 4,
+                _ => (),
+            }
+        }
+        x == 0 && y == 0 || direction != 0
     }
 }
 
+#[test]
+fn test_is_robot_bounded() {
+    let expected = true;
+    let actual = Solution::is_robot_bounded(String::from("GGLLGG"));
+    assert!(actual == expected, "Expected {:?}, but got {:?}", expected, actual);
+}
